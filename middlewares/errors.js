9c3add1bf2bc90a4ad1894289965430e124c8914
Sergey Borodulin <sergei1234565@yandex.ru>
2021-10-27 10:05:08 +0300

    исправлена ошибка с 500 на 401

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -3,7 +3,7 @@ module.exports = (err, req, res, next) => {
     const err = new Error('Переданы некорректные данные');
     res.status(400).send({ message: err.message });
   }
-  const status = err.statusCode || 500;
+  const status = err.statusCode || 401;
   // const { message } = err;
   console.log(err.message);
   res.status(status).json({ message: err.message });
