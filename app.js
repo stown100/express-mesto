1be70a48c0c8297df260ad12ae77c86383ff1738
Sergey Borodulin <sergei1234565@yandex.ru>
2021-11-06 22:02:08 +0300

    Вернул всё как было 4го числа

--- app.js
+++ app.js
@@ -1,14 +1,111 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const { celebrate, Joi } = require('celebrate');
+// const validator = require('validator');
+// const cors = require('cors');
+// const routesCards = require('./routes/cards');
+// const routesUsers = require('./routes/users');
+// const { createUser, login } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
+// const errors = require('./middlewares/errors');
+// // const { allowOrigin } = require('./middlewares/cors');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
+// // const bodyParser = require('body-parser');
+// // const path = require('path');
+
+// const PORT = 3000;
+// const app = express();
+
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useNewUrlParser: true,
+//   autoIndex: true, // make this also true
+// });
+
+// // app.use(express.static(path.join(__dirname, 'public')));
+
+// const corsOptions = {
+//   origin: [
+//     'https://application-mesto.nomoredomains.icu',
+//     'http://application-mesto.nomoredomains.icu',
+//     'https://api.application-mesto.nomoredomains.xyz',
+//     'http://api.application-mesto.nomoredomains.xyz',
+//     'localhost:3000',
+//   ],
+//   methods: ['PUT', 'GET', 'POST', 'PATCH', 'DELETE', 'HEAD'],
+//   preflightContinue: false,
+//   optionSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+//   credentials: true,
+// };
+
+// app.use((req, res, next) => {
+//   res.header({ 'Access-Control-Allow-Origin': '*' });
+//   next();
+// });
+
+// app.use('*', cors(corsOptions));
+
+// app.use(express.json());
+
+// app.use(requestLogger); // подключаем логгер запросов
+
+// // app.use('*', cors(allowOrigin));
+
+// // app.use(cors);
+
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().email({ tlds: { allow: false } }).required(),
+//     password: Joi.string().required(),
+//   }).unknown(true),
+// }), login);
+
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().email({ tlds: { allow: false } }).required(),
+//     password: Joi.string().required(),
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string().custom((value, helper) => {
+//       if (validator.isURL(value, { require_protocol: true })) {
+//         return value;
+//       }
+//       return helper.message('Невалидный url');
+//     }),
+//   }),
+// }), createUser);
+
+// app.use('/users', auth, routesUsers);
+// app.use('/cards', auth, routesCards);
+
+// app.all('*', (req, res, next) => {
+//   const err = new Error('Ресурс не найден');
+//   err.statusCode = 404;
+//   return next(err);
+// });
+
+// app.use(errorLogger); // подключаем логгер ошибок
+
+// app.use(errors);
+
+// // app.use((err, req, res, next) => {
+// //   res.send({ msg: err.message });
+// //   next(new Error('Ошибка авторизации'));
+// // });
+
+// app.listen(PORT, () => {
+//   console.log('Ссылка на сервер: http://api.application-mesto.nomoredomains.xyz');
+// });
 const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
 const validator = require('validator');
-const cors = require('cors');
 const routesCards = require('./routes/cards');
 const routesUsers = require('./routes/users');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errors = require('./middlewares/errors');
-// const { allowOrigin } = require('./middlewares/cors');
+const { allowOrigin } = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const bodyParser = require('body-parser');
 // const path = require('path');
@@ -23,35 +120,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-  origin: [
-    'https://application-mesto.nomoredomains.icu',
-    'http://application-mesto.nomoredomains.icu',
-    'https://api.application-mesto.nomoredomains.xyz',
-    'http://api.application-mesto.nomoredomains.xyz',
-    'localhost:3000',
-  ],
-  methods: ['PUT', 'GET', 'POST', 'PATCH', 'DELETE', 'HEAD'],
-  preflightContinue: false,
-  optionSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
-
-app.use((req, res, next) => {
-  res.header({ 'Access-Control-Allow-Origin': '*' });
-  next();
-});
-
-app.use('*', cors(corsOptions));
-
 app.use(express.json());
 
 app.use(requestLogger); // подключаем логгер запросов
 
-// app.use('*', cors(allowOrigin));
-
-// app.use(cors);
+app.use(allowOrigin);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -75,8 +148,10 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use('/users', auth, routesUsers);
-app.use('/cards', auth, routesCards);
+app.use(auth);
+
+app.use('/users', routesUsers);
+app.use('/cards', routesCards);
 
 app.all('*', (req, res, next) => {
   const err = new Error('Ресурс не найден');
@@ -94,5 +169,5 @@ app.use(errors);
 // });
 
 app.listen(PORT, () => {
-  console.log('Ссылка на сервер: http://api.application-mesto.nomoredomains.xyz');
+  console.log('Ссылка на сервер: http://application-mesto.nomoredomains.icu/');
 });
