5c6c36722bea29c11d830bff28777fce2c9af5fe
Sergey Borodulin <sergei1234565@yandex.ru>
2021-11-14 18:30:24 +0300

    Убрал required у email

--- app.js
+++ app.js
@@ -59,14 +59,14 @@ app.get('/crash-test', () => {
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    email: Joi.string().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    email: Joi.string().email({ tlds: { allow: false } }),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
